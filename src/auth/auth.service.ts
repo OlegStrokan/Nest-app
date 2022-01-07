e5315b214a9024eaa96fe877b708cac95c53ef68
olehstrokan <oleh@wattstor.com>
2022-01-07 20:04:45 +0100

    Version 1.3.1. Login functional, new readme

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Body, HttpException, HttpStatus, Injectable, Post } from '@nestjs/common';
+import { Body, HttpException, HttpStatus, Injectable, Post, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
@@ -10,7 +10,8 @@ export class AuthService {
   constructor(private userService: UsersService, private jwtService: JwtService) {}
 
   async login(userDto: CreateUserDto) {
-
+    const user = await this.validateUser(userDto)
+    return this.generateToken(user)
   }
 
   async registration(userDto: CreateUserDto) {
@@ -24,10 +25,20 @@ export class AuthService {
     return this.generateToken(user)
   }
 
-  async generateToken(user: User) {
+  private async generateToken(user: User) {
     const payload = { email: user.email, id: user.id, roles: user.roles }
     return {
       token: this.jwtService.sign(payload)
     }
   }
+
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email)
+    const passwordEquals = await bcrypt.compare(userDto.password, user.password)
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({ message: 'Некорректный email или пароль'})
+  }
 }
+
