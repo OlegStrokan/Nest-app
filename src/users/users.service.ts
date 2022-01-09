fe6141923774545f2d0b7922a394972d6c4e1114
olehstrokan <oleh@wattstor.com>
2022-01-09 11:14:09 +0100

    Version 1.4.0. Ban functional

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,33 +14,41 @@ export class UsersService {
   async createUser(dto: CreateUserDto) {
     const user = await this.userRepository.create(dto);
     const role = await this.roleService.getRoleByValue('USER');
-    await user.$set('roles', [role.id])
-    user.roles = [role]
+    await user.$set('roles', [role.id]);
+    user.roles = [role];
     return user;
   }
 
   async getAllUsers() {
-    const users = await this.userRepository.findAll({ include: {all: true}});
+    const users = await this.userRepository.findAll({ include: { all: true } });
     return users;
   }
 
   async getUserByEmail(email: string) {
-    const user = await this.userRepository.findOne({ where: { email }, include: {all: true}})
+    const user = await this.userRepository.findOne({ where: { email }, include: { all: true } });
     return user;
   }
 
   async addRole(dto: AddRoleDto) {
-      const user = await this.userRepository.findByPk(dto.userId);
-      const role = await this.roleService.getRoleByValue(dto.value)
+    const user = await this.userRepository.findByPk(dto.userId);
+    const role = await this.roleService.getRoleByValue(dto.value);
     if (role && user) {
       // $add - аналог append
       await user.$add('role', role.id);
-      return dto
+      return dto;
     }
-    throw new HttpException('Пользователь или роль не найдены', HttpStatus.NOT_FOUND)
+    throw new HttpException('Пользователь или роль не найдены', HttpStatus.NOT_FOUND);
   }
 
   async ban(dto: BanUserDto) {
-
+    const user = await this.userRepository.findByPk(dto.userId);
+    if (!user) {
+      throw new HttpException('Пользователь не найден', HttpStatus.NOT_FOUND)
+    }
+    user.banned = true;
+    user.banReason = dto.banReason;
+    // обновляем значения в базе данных
+    await user.save()
+    return user;
   }
 }
